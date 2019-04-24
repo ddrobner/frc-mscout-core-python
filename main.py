70a576b29132ca9593833c7a9bf3df31202d35b8
David Drobner <david@drobner.ca>
2019-04-24 11:36:58 -0400

    Quits after scanning

--- main.py
+++ main.py
@@ -1,17 +1,63 @@
-import pyzbar
+import pyzbar.pyzbar as pyzbar
 import cv2
 import sys
 import time
+import numpy as np
 
 camera = cv2.VideoCapture(0)
+
+def decode(im) :
+  # Find barcodes and QR codes
+  decodedObjects = pyzbar.decode(im)
+
+  # Print results
+  for obj in decodedObjects:
+    print('Type : ', obj.type)
+    print('Data : ', obj.data,'\n')
+
+  return decodedObjects
+
+
+# Display barcode and QR code location
+def display(im, decodedObjects):
+
+  # Loop over all decoded objects
+  for decodedObject in decodedObjects:
+    points = decodedObject.polygon
+
+    # If the points do not form a quad, find convex hull
+    if len(points) > 4 :
+      hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+      hull = list(map(tuple, np.squeeze(hull)))
+    else :
+      hull = points;
+
+    # Number of points in the convex hull
+    n = len(hull)
+
+    # Draw the convext hull
+    for j in range(0,n):
+      cv2.line(im, hull[j], hull[ (j+1) % n], (255,0,0), 3)
+
+  # Display results
+  cv2.imshow("Results", im);
+  cv2.waitKey(0);
 
 while True:
     ret, frame = camera.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
+
+
+    decodedCode = decode(frame)
+    #display(frame, decodedCode)
+
+    if decodedCode:
+        break
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 camera.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
