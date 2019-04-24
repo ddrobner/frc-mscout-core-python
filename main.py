9c2270ecbe0344ff47496977a298e4abc9f4d6c9
David Drobner <david@drobner.ca>
2019-04-24 11:41:35 -0400

    Remove unused display function

--- main.py
+++ main.py
@@ -18,31 +18,6 @@ def decode(im) :
   return decodedObjects
 
 
-# Display barcode and QR code location
-def display(im, decodedObjects):
-
-  # Loop over all decoded objects
-  for decodedObject in decodedObjects:
-    points = decodedObject.polygon
-
-    # If the points do not form a quad, find convex hull
-    if len(points) > 4 :
-      hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
-      hull = list(map(tuple, np.squeeze(hull)))
-    else :
-      hull = points;
-
-    # Number of points in the convex hull
-    n = len(hull)
-
-    # Draw the convext hull
-    for j in range(0,n):
-      cv2.line(im, hull[j], hull[ (j+1) % n], (255,0,0), 3)
-
-  # Display results
-  cv2.imshow("Results", im);
-  cv2.waitKey(0);
-
 while True:
     ret, frame = camera.read()
 
@@ -51,7 +26,7 @@ while True:
 
 
     decodedCode = decode(frame)
-    #display(frame, decodedCode)
+
 
     if decodedCode:
         break
