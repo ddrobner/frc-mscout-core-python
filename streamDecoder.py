53fa3e82169619f090f1538b6f0d0779cca33302
David Drobner <david@drobner.ca>
2019-05-24 13:18:11 -0400

    Created a stream decoder class

---
+++ streamDecoder.py
@@ -0,0 +1,12 @@
+class streamDecoder:
+    dataArr = []
+    codeArr = []
+    qrHash = []
+
+    def __init__():
+        this.reset()
+
+    def reset():
+        this.dataArr = []
+        this.codeArr = []
+        this.qrHash = []
